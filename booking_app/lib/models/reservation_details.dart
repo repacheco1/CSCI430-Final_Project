2be5634b7e1337f9805186f75918faaeee321ba1
miguel Rosales <miguelrosales@miguels-MacBook-Pro.local>
2020-12-08 22:24:01 -0800

    added firebase functionality

---
+++ booking_app/lib/models/reservation_details.dart
@@ -0,0 +1,22 @@
+
+class Reservation {
+  final String uid;
+  // ignore: non_constant_identifier_names
+  final String restaurant_name;
+  // ignore: non_constant_identifier_names
+  final String reservation_time;
+  // ignore: non_constant_identifier_names
+  final String reservation_date;
+
+
+  // ignore: sort_constructors_first
+  Reservation({
+    this.uid,
+    // ignore: non_constant_identifier_names
+    this.restaurant_name,
+    // ignore: non_constant_identifier_names
+    this.reservation_time,
+    // ignore: non_constant_identifier_names
+    this.reservation_date,
+  });
+}
